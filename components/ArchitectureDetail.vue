0f9a14bfd26f47ec6a44bff5675675b9c96a4908
ScGong0504 <sichengong0504@gmail.com>
2022-10-22 16:17:51 +0800

    merge master into MGS-gsc

---
+++ components/ArchitectureDetail.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="detail-title">{{ title }}</div>
+        <div class="detail-text">{{ text }}</div>
+        <img :src="`${fig}`" class="detail-fig" />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ArchitectureDetail",
+        props: [
+            "title",
+            "text",
+            "fig"
+        ]
+    }
+</script>
+
+<style scoped>
+.detail-title {
+    text-align: center;
+    margin-top: 40px;
+    font-size: 22px;
+    font-weight: bold;
+}
+.detail-text {
+    margin-left: 15%;
+    margin-right: 15%;
+    margin-top: 20px;
+    font-size: 18px;
+}
+.detail-fig {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 20px;
+    margin-bottom: 40px;
+}
+</style>
